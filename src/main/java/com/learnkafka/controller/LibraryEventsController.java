300cc6b6a731a9e2637acff77fff936700beb049
LuisGuiCv <luisguillermocruzvargas99@gmail.com>
2023-09-16 14:37:23 -0600

    synchronous library event with kafka

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,8 +1,11 @@
 package com.learnkafka.controller;
 
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.producer.Producer;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class LibraryEventsController {
+
+
+    @Autowired
+    private Producer producer;
+    @SneakyThrows
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
         log.info("libraryEvent: {}",libraryEvent);
+        producer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
